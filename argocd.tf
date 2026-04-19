6a690c9e06f9c5ed3af265abd06032349f86506f
Alexandre St-fort <solonoobhacker6767+@gmail.com>
2026-04-19 02:29:53 -0400

    Update argocd.tf

--- argocd.tf
+++ argocd.tf
@@ -7,29 +7,4 @@ resource "helm_release" "argocd" {
   version          = "v3.3.2"
 
     values = [file("values/argocd.yaml")]
-
-#  values = [<<-EOT
-#      server:
-#        service:
-#          type: NodePort  # Changed from LoadBalancer
-      
-      # Inject the Ingress configuration directly into the chart
-#        ingress:
-#          enabled: true
-#          ingressClassName: gce
-#          annotations:
-#            kubernetes.io/ingress.global-static-ip-name: "lb-static-ip"
-#            ingress.gcp.kubernetes.io/pre-shared-cert: "argocd-cert"
-#            kubernetes.io/ingress.allow-http: "false"
-#          hosts:
-#            - argocd.example.com
-#      
-#        params:
-#          server.insecure: true # Required for GCLB backends
-#
-#      global:
-#        domain: argocd.example.com
-#    EOT
-#  ]
-
 }
